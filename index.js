d66e2aaf721fedf27b97af940c0b738d8608a364
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-12-12 00:35:42 +0600

    added specific task delete api

--- index.js
+++ index.js
@@ -81,6 +81,25 @@ async function run() {
         res.status(500).json({ error: "Internal Server Error" });
       }
     });
+
+    app.delete("/tasks/:id", async (req, res) => {
+      const taskId = req.params.id;
+
+      try {
+        const result = await tasksCollection.deleteOne({
+          _id: new ObjectId(taskId),
+        });
+
+        if (result.deletedCount === 0) {
+          res.status(400).json({ error: "Task not found" });
+        } else {
+          res.json({ message: "Task deleted successfully" });
+        }
+      } catch (err) {
+        console.error("Error deleting task:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
